a62e27dd81c70b5a46da75dda36cc84ed64d980f
Sabyrzhan Zhaslanuly <verosabyrka@gmail.com>
2026-09-02 16:31:14 +0600

    chore: set up path aliases

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import './style.css'
-import heroImg from './assets/hero.png'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import { setupCounter } from './counter.js'
+import '@/style.css'
+import heroImg from '@/assets/hero.png'
+import javascriptLogo from '@/assets/javascript.svg'
+import viteLogo from '@/assets/vite.svg'
+import { setupCounter } from '@/counter.js'
 
 document.querySelector('#app').innerHTML = `
 <section id="center">
